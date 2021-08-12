4d8f6b6409e4890498b4fac4e5e06dc154207a16
KonstantIMP <mihedovkos@gmail.com>
2021-08-12 14:48:26 +0300

    Add tmpStorage class for sticker storage

---
+++ source/kimp/storage.d
@@ -0,0 +1,29 @@
+/**
+ * Module for tmp files managing
+ * Author: KonstantIMP <mihedovkos@gmail.com>
+ * Date: 12 Aug 2021
+ */
+module kimp.storage;
+
+import std.file, std.uuid, std.path;
+
+/** 
+ * Class for working with tmp files
+ */
+class TmpStorage {
+    /** 
+     * Generate unused name for tmp file
+     * Returns: Generated filename
+     */
+    static public string genTmpFile () {
+        string tmpDir = tempDir();
+
+        string fileName = randomUUID().toString() ~ ".tmp";
+
+        while (exsist (tmpDir ~ pathSeparator ~ fileName)) {
+            fileName = randomUUID().toString() ~ ".tmp";
+        }
+
+        return fileName;
+    }
+}
