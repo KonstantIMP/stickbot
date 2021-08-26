58938fce76bd0fffadfde271dc95048d6da2beda
KonstantIMP <mihedovkos@gmail.com>
2021-08-26 13:01:28 +0300

    Add Ctrl+C handling

--- source/app.d
+++ source/app.d
@@ -1,12 +1,18 @@
-import tg.bot, std.stdio, kimp.stickbot;
+import tg.bot, kimp.log, kimp.stickbot, kimp.exit;
 
 void main (string [] args) {
+    StickBotLogger logger = new StickBotLogger();
+
     if (args.length != 2) {
-        stderr.writeln ("[ERROR] Incorrect input data.");
-        stderr.writeln ("[ERROR] [USAGE] stickbot <bot_api>");
-        return;
+        logger.error ("Incorrect input data.");
+        logger.warning ("USAGE] stickbot <bot_api>");
     } else {
-        StickBot stick = new StickBot(args[1]);
-        stick.loop ();
+        exitScope (() {
+            StickBot stick = new StickBot(args[1]);
+            stick.loop ();
+        }, () nothrow @nogc @system {
+            import core.stdc.stdio;
+            printf ("\033[33;1m[WARNING]\033[0m Catched Ctrl+C. Exit.\n");    
+        });
     }
 }
--- source/kimp/exit.d
+++ source/kimp/exit.d
@@ -5,3 +5,27 @@
  */
 module kimp.exit;
 
+private import core.stdc.stdlib;
+
+private void delegate() nothrow @nogc _ccallback;
+private extern (C) void handle (int sig) nothrow @nogc @system { _ccallback(); exit (0); }
+
+/** 
+ * Runs delegate in the scoped space
+ * Params:
+ *   task = Delegate for execute
+ *   callback = Delegate for run befire exit
+ */
+public void exitScope (void delegate() task, void delegate() nothrow @nogc @system callback) {
+    version (Windows) {
+        scope (exit) { callback(); }
+        task();
+    }
+    else {
+        import core.sys.posix.signal;
+
+        _ccallback = callback; signal(2, &handle);
+
+        task();
+    }
+} 
